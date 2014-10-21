b37043c88cfc8a1017a11842e2e7ac53f5d0ee63
Mac <Mac@Macs-iMac.local>
2014-10-20 18:06:26 -0700

    Initial Commit

---
+++ ScanBarSOURCECODE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanBarSOURCECODE
+//
+//  Created by Mac on 10/20/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
